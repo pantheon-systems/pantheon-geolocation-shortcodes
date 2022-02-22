3a960df9c1212ead01b18046d62b74cef3086c13
Chris Reynolds <chris@jazzsequence.com>
2022-02-22 14:25:04 -0700

    set up some dummy data

--- tests/shortcodesTest.php
+++ tests/shortcodesTest.php
@@ -14,6 +14,26 @@ use PHPUnit\Framework\TestCase;
  */
 class shortcodesTests extends TestCase {
 	/**
+	 * Setup the test suite.
+	 */
+	function setUp() : void {
+		add_filter( 'pantheon.ei.parsed_geo_data', function() {
+			return [
+				'country' => 'US',
+				'city' => 'Salt Lake City',
+				'region' => 'UT',
+				'continent' => 'NA',
+				'conn-speed' => 'broadband',
+				'conn-type' => 'wired',
+				'lat' => '40.7',
+				'lon' => '-111.9',
+			];
+		} );
+
+		parent::setUp();
+	}
+
+	/**
 	 * Test that the plugin is loaded.
 	 */
 	public function testBootstrap() {
